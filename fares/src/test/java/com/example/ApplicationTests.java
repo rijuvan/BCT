08e38716158af5554aa6e5defded3504e7cca591
Rijuvan.Ansari <rijuvanansari@yahoo.com>
2018-03-22 11:33:23 +0530

    ---Fare services !!

---
+++ fares/src/test/java/com/example/ApplicationTests.java
@@ -0,0 +1,23 @@
+/*package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.brownfield.pss.fares.Application;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootApplication
+@WebAppConfiguration
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
