9b965350593a6ddb81881df70497b75eda550673
Rijuvan.Ansari <rijuvanansari@yahoo.com>
2018-03-22 11:15:19 +0530

    --- Checkin service !!

---
+++ checkin/src/test/java/com/example/ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.brownfield.pss.checkin.Application;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootConfiguration
+@WebAppConfiguration
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
